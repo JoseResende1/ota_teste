d31af61df92ede85a2bbccaa4f90677660627e77
JoseResende1 <jose.cristiano_12@hotmail.com>
2026-01-05 23:46:04 +0000

    V3.0.0

--- ota/main.py
+++ ota/main.py
@@ -2,7 +2,7 @@
 # main.py
 # ======================================================
 from app.controller import Controller
-print("RUN V2.0.0")
+print("RUN V3.0.0")
 def run():
     c = Controller()
     c.loop()
